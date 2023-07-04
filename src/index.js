75ac36a736323352ec0e4152c1650fdc50d94ed0
AlixiaDae <lizziejones1208@gmail.com>
2023-07-04 14:14:59 +0800

    Make HTML

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//DOM Elements
+const studentForm = document.getElementById("student-form");
+const students= document.querySelector(".students");
+const nameInput = studentForm['name'];
+const ageInput = studentForm['age'];
+const numberInput = studentForm['number'];
